75d65153ffcc8f5f2739dcde1b6f44a27419d1c4
Ori Munk <oriko1500@gmail.com>
2019-11-30 14:55:13 +0200

    Added day 1 solution

---
+++ 2018/day1/Day1/Day1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string path = args[0];
+			string[] frequencies = File.ReadAllLines(path);
+			int current_frequency = 0;
+			foreach(string freq in frequencies)
+			{
+				current_frequency += int.Parse(freq);
+			}
+			Console.WriteLine(current_frequency);
+		}
+	}
+}
